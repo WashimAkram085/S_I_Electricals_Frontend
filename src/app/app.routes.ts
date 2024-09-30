fa4d5642507efb82197c881184604b02d4e27506
WashimAkram085 <washimakram112001@gmail.com>
2024-10-01 01:07:11 +0530

    productdetails done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { ProductlistComponent } from './productlist/productlist.component';
 import { HomeComponent } from './home/home.component';
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
 
 export const routes: Routes = [
     {path:'register', component: RegisterComponent},
     {path:'login', component:LoginComponent},
     {path:'productlist',component:ProductlistComponent},
-    {path:'home',component:HomeComponent}
+    {path:'home',component:HomeComponent},
+    {path:'product-details',component:ProductdetailsComponent}
 ];

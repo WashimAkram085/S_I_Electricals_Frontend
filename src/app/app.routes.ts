d0feda5f86e1c4da2dff8a2a7d39164dde1b9e62
WashimAkram085 <washimakram112001@gmail.com>
2024-10-01 01:00:36 +0530

    home component done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { ProductlistComponent } from './productlist/productlist.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:'register', component: RegisterComponent},
     {path:'login', component:LoginComponent},
     {path:'productlist',component:ProductlistComponent},
+    {path:'home',component:HomeComponent}
 ];
--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -10,6 +10,17 @@ import { CommonModule } from '@angular/common';
 })
 export class PaginationComponent {
 
-  
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+  @Output() pageChange = new EventEmitter<number>();
+
+
+  get pageNumbers(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1)
+  }
+
+  changePage(page: number): void {
+    this.pageChange.emit(page)
+  }
 
 }

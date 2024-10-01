312c19a76a912e3121917988be3af0867f17cbff
Washim Akram <Washim.Akram@godigit.com>
2024-10-01 16:17:08 +0530

    category products component done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,16 @@ import { ProductlistComponent } from './productlist/productlist.component';
 import { HomeComponent } from './home/home.component';
 import { ProductdetailsComponent } from './productdetails/productdetails.component';
 import { CategoryComponent } from './category/category.component';
+import { CategoryproductsComponent } from './categoryproducts/categoryproducts.component';
 
 export const routes: Routes = [
+    // Public routes
     {path:'register', component: RegisterComponent},
     {path:'login', component:LoginComponent},
     {path:'productlist',component:ProductlistComponent},
     {path:'home',component:HomeComponent},
     {path:'product/:productId',component:ProductdetailsComponent},
     {path:'category', component: CategoryComponent},
+    {path: 'products/:categoryId', component: CategoryproductsComponent},
     
 ];
